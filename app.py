f06d562aae511751ad3f406302bab459cb2b4b5a
igor <geos79@tut.by>
2021-01-13 16:10:57 +0300

    add del and update article

--- app.py
+++ app.py
@@ -14,13 +14,12 @@ class Article(db.Model):
     intro = db.Column(db.String(300), nullable=False)
     text = db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow())
+
     def __repr__(self):
         return "<Article %r>" % self.id
 
 
-
 @app.route('/')
-@app.route('/home')
 def index():
     return render_template('index.html')
 
@@ -41,6 +40,20 @@ def post_detail(id):
     article = Article.query.get(id)
     return render_template('post_detail.html', article=article)
 
+
+@app.route('/posts/<int:id>/del')
+def post_delete(id):
+    article = Article.query.get_or_404(id)
+
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/posts')
+
+    except:
+        return 'При добавлении статьи произошла ошибка('
+
+
 @app.route('/create-article', methods=["POST", "GET"])
 def create_article():
     if request.method == "POST":
@@ -59,6 +72,24 @@ def create_article():
     else:
         return render_template('create-article.html')
 
+
+@app.route('/posts/<int:id>/update', methods=["POST", "GET"])
+def post_update(id):
+    article = Article.query.get_or_404(id)
+    if request.method == "POST":
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
+        article.date = datetime.utcnow()
+
+        try:
+            db.session.commit()
+            return redirect('/posts')
+
+        except:
+            return 'При редактировании статьи произошла ошибка('
+    else:
+        return render_template('post_update.html', article=article)
 
 if __name__ == "__main__":
     app.run(debug=True)
